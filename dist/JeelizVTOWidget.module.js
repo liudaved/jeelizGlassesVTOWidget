a93b0d1ea2497214ae41a3957b3cd2bbc3dc0f0c
xavierjs <xavier.bourry@polytechnique.org>
2023-01-06 10:29:10 -0500

    [QUAL] Improve shadow resolution, fix shadow bug with ski masks

--- dist/JeelizVTOWidget.module.js
+++ dist/JeelizVTOWidget.module.js
@@ -16,6 +16,42 @@
  * limitations under the License.
 */
 
+ /**
+ * Jeeliz Glasses VTO Widget - https://github.com/jeeliz/jeelizGlassesVTOWidget
+ *
+ * Copyright 2020 WebAR.rocks ( https://webar.rocks )
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+ /**
+ * Jeeliz Glasses VTO Widget - https://github.com/jeeliz/jeelizGlassesVTOWidget
+ *
+ * Copyright 2020 WebAR.rocks ( https://webar.rocks )
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 /* eslint-disable */
 let JEELIZVTO=null;let JEELIZVTOWIDGET=null;var $jscomp=$jscomp||{};$jscomp.scope={};$jscomp.owns=function(ra,wa){return Object.prototype.hasOwnProperty.call(ra,wa)};$jscomp.ASSUME_ES5=!1;$jscomp.ASSUME_NO_NATIVE_MAP=!1;$jscomp.ASSUME_NO_NATIVE_SET=!1;$jscomp.SIMPLE_FROUND_POLYFILL=!1;$jscomp.ISOLATE_POLYFILLS=!1;$jscomp.defineProperty=$jscomp.ASSUME_ES5||"function"==typeof Object.defineProperties?Object.defineProperty:function(ra,wa,Ha){if(ra==Array.prototype||ra==Object.prototype)return ra;ra[wa]=Ha.value;return ra};
 $jscomp.getGlobal=function(ra){ra=["object"==typeof globalThis&&globalThis,ra,"object"==typeof window&&window,"object"==typeof self&&self,"object"==typeof global&&global];for(var wa=0;wa<ra.length;++wa){var Ha=ra[wa];if(Ha&&Ha.Math==Math)return Ha}throw Error("Cannot find global object");};$jscomp.global=$jscomp.getGlobal(this);$jscomp.IS_SYMBOL_NATIVE="function"===typeof Symbol&&"symbol"===typeof Symbol("x");$jscomp.TRUST_ES6_POLYFILLS=!$jscomp.ISOLATE_POLYFILLS||$jscomp.IS_SYMBOL_NATIVE;
